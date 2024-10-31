e4772664105290359cdf386fb3024556647723e6
sarimultv <sarimullaskar.shl@gmail.com>
2024-10-31 14:58:56 +0530

    Little Changes with theme icons

--- src/components/RightSideNav.jsx
+++ src/components/RightSideNav.jsx
@@ -1,9 +1,10 @@
 import { IoHome } from "react-icons/io5";
+import { GoSun } from "react-icons/go";
 import { FaMoon } from "react-icons/fa";
 import { FaUser } from "react-icons/fa";
 import { MdLocalPostOffice } from "react-icons/md";
 import { Link } from "react-router-dom";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { initialMode } from "../store/themeSlice";
 
 const RightSideNav = () => {
@@ -13,34 +14,36 @@ const RightSideNav = () => {
     dispatch(initialMode());
   };
 
+  const mode = useSelector((store) => store.appTheme.mode);
+
   return (
-    <div className="w-[5%] pr-2">
+    <div className="w-[5%] mr-10">
       <ul>
         <Link to={"/"}>
-          <li className="flex justify-center items-center mb-2 p-5 bg-gray-100 text-black rounded-full hover:bg-yellow-500 hover:text-white">
+          <li className="flex justify-center items-center mb-2 p-5 bg-gray-200 text-black rounded-full hover:bg-yellow-500 hover:text-white">
             <span className="hidden">Home</span>
             <IoHome size={20} />
           </li>
         </Link>
 
         <Link to={"/about"}>
-          <li className="flex justify-center items-center p-5 bg-gray-100 text-black rounded-full hover:bg-yellow-500 hover:text-white">
+          <li className="flex justify-center items-center p-5 bg-gray-200 text-black rounded-full hover:bg-yellow-500 hover:text-white">
             <span className="hidden">About</span>
             <FaUser size={20} />
           </li>
         </Link>
 
         <Link to={"/contact"}>
-          <li className="flex justify-center items-center my-2 p-5 bg-gray-100 text-black rounded-full hover:bg-yellow-500 hover:text-white">
+          <li className="flex justify-center items-center my-2 p-5 bg-gray-200 text-black rounded-full hover:bg-yellow-500 hover:text-white">
             <span className="hidden">Contact</span>
             <MdLocalPostOffice size={20} />
           </li>
         </Link>
         <li
-          className="flex justify-center items-center p-5 bg-gray-100 text-black rounded-full cursor-pointer hover:bg-yellow-500 hover:text-white"
+          className="flex justify-center items-center p-5 bg-gray-200 text-black rounded-full cursor-pointer hover:bg-yellow-500 hover:text-white"
           onClick={handleAppTheme}
         >
-          <FaMoon size={20} />
+          {mode === "white" ? <FaMoon size={20} /> : <GoSun size={20} />}
         </li>
       </ul>
     </div>
