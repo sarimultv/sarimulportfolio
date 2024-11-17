90dd4abfe6fb48afc19be9e9b770f69ebe6e8f7f
sarimultv <sarimullaskar.shl@gmail.com>
2024-11-17 22:59:20 +0530

    Added project description and Intro description

--- src/components/BannerPhoto.jsx
+++ src/components/BannerPhoto.jsx
@@ -4,7 +4,7 @@ const BannerPhoto = () => {
   return (
     <div className="md:w-[45%] flex justify-center py-4">
       <img
-        className="md:rounded-3xl md:w-[80%] md:h-[35rem] w-[60%] rounded-full md:border-none border-4 border-gray-800 shadow-lg object-cover"
+        className="md:rounded-3xl md:w-[80%] md:h-[35rem] md:border-none w-[60%] rounded-full border-4 border-gray-800 shadow-lg object-cover"
         src={PROFILE_PICTURE_LINK}
         alt="Users' profile picture"
       />
